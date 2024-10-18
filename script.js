cae9b968b7e31dadcdafae68a1d6b983557a0798
BreidysG <Breidysgutierrez8@gmail.com>
2024-10-18 14:32:33 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,184 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const titleInit = document.getElementById("titleInit");
+    const paragraphInit = document.getElementById("paragraphInit");
+    const startButton = document.getElementById('startButton');
+    const textContainer = document.getElementById('textContainer');
+    const finishButton = document.getElementById('finishButton');
+    const questionnaire = document.getElementById('questionnaire');
+    const quizForm = document.getElementById('quizForm');
+    const results = document.getElementById('results');
+    const questionOptions = document.querySelector('.question-options'); // Contenedor de las opciones de pregunta
+    const timerValue = document.getElementById('timerValue');
+    let time = [];
+
+    let wordCount = 0;
+    let startTime, endTime;
+    let quizCompleted = false;
+    let timerInterval;
+
+    // Texto para las preguntas del cuestionario
+    const questions = [
+        {
+            question: "¿Qué inventó el papá para callar los gritos de su familia?", 
+            answers: [
+                "Un traga gritos", 
+                "Una mosquizampa", 
+                "Un envase para atrapar gritos", 
+                "Un imán atrapa gritos"
+            ], 
+            correctAnswer: "Una mosquizampa"
+        },
+        {
+            question: "¿Quiénes son los personajes de la historia?", 
+            answers: [
+                "Mamá Bocca, papá Cayo, e hija Chilla", 
+                "Un vecindario", 
+                "Mamá, tía y sobrino", 
+                "Mamá, hijo y la vecina"
+            ], 
+            correctAnswer: "Mamá Bocca, papá Cayo, e hija Chilla"
+        },
+        {
+            question: "¿Chilla qué tuvo que hacer para poder comer sin que se alimentaran de sus gritos los mosquizampa?", 
+            answers: [
+                "Hablar muy bajito", 
+                "Hacer señas", 
+                "Comunicarse por mensajes de texto", 
+                "Utilizar el lenguaje Braille"
+            ], 
+            correctAnswer: "Hablar muy bajito"
+        },
+        {
+            question: "¿Qué pasó con el invento de los mosquizampa?", 
+            answers: [
+                "Se murieron de hambre", 
+                "Los destruyó el papá cuando dejaron de cumplir el objetivo", 
+                "Se fueron por el mundo a seguir comiendo gritos", 
+                "Se quedaron como mascotas en el hogar"
+            ], 
+            correctAnswer: "Se fueron por el mundo a seguir comiendo gritos"
+        },
+        {
+            question: "¿Qué enseñanza deja la lectura?", 
+            answers: [
+                "Los gritos destruyen el entorno, con un poco de calma se pueden conseguir más cosas", 
+                "No es necesario gritar, se pueden pedir las cosas de buena manera", 
+                "Con la ira no se consigue nada bueno", 
+                "Todas las respuestas son correctas"
+            ], 
+            correctAnswer: "Todas las respuestas son correctas"
+        }
+    ];
+    
+              
+    // Función para contar las palabras en el texto
+    function countWords(text) {
+        return text.split(/\s+/).length;
+    }
+
+    // Función para calcular la velocidad de lectura
+    function calculateReadingSpeed(startTime, endTime, wordCount) {
+        const minutes = (endTime - startTime) / 60000; // Convertir a minutos
+        return Math.round(wordCount / minutes);
+    }
+
+    // Función para mostrar las preguntas del cuestionario
+    function displayQuestions() {
+        const questionList = document.getElementById('questionList');
+        questionList.innerHTML = '';
+        
+        questions.forEach((question, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <p>${question.question}</p>
+                <div class="question-options">
+                    ${question.answers.map((answer, i) => `
+                        <label>
+                            <input type="radio" name="answer${index}" value="${answer}">
+                            ${answer}
+                        </label>
+                    `).join('')}
+                </div>
+            `;
+            questionList.appendChild(listItem);
+        });
+    }
+
+    // Evento para comenzar el test al hacer clic en "Empezar"
+    startButton.addEventListener('click', function() {
+        textContainer.classList.remove('hidden');
+        paragraphInit.classList.add("hidden");
+        startButton.classList.add('hidden');
+        startTime = Date.now();
+        // Iniciar el contador
+        timerInterval = setInterval(updateTimer, 1000);
+    });
+
+    // Función para actualizar el contador de tiempo
+    function updateTimer() {
+        const currentTime = Math.floor((Date.now() - startTime) / 1000); // Tiempo transcurrido en segundos
+        timerValue.textContent = currentTime;
+        time.push(currentTime);
+    }
+
+    // Evento para finalizar el test al hacer clic en "Terminar Test"
+    finishButton.addEventListener('click', function() {
+        clearInterval(timerInterval); // Detener el contador
+        titleInit.classList.add("hidden");
+        textContainer.classList.add('hidden');
+        finishButton.classList.add('hidden');
+        endTime = Date.now();
+        wordCount = countWords(document.getElementById('textToRead').textContent);
+        const readingSpeed = calculateReadingSpeed(startTime, endTime, wordCount);
+        questionnaire.classList.remove('hidden');
+        displayQuestions();
+    });
+
+    // Evento para enviar el cuestionario
+    quizForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (!quizCompleted) {
+            let anyAnswerSelected = true; // Inicialmente, asumimos que al menos una opción está seleccionada
+
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (!selectedAnswer) { // Si ninguna opción está seleccionada para alguna pregunta
+                    anyAnswerSelected = false;
+                    return;
+                }
+            });
+
+            if (!anyAnswerSelected) {
+                alert("Debes seleccionar una opción para cada pregunta antes de terminar.");
+                return;
+            }
+
+            let correctAnswers = 0;
+            let totalQuestions = questions.length;
+            
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (selectedAnswer && selectedAnswer.value === question.correctAnswer) {
+                    correctAnswers++;
+                }
+            });
+            
+            let comprehensionPercentage = (correctAnswers / totalQuestions) * 100;
+            let timeResult = time[time.length-1];
+            
+            quizCompleted = true;
+            results.classList.remove('hidden');
+            questionnaire.classList.add('hidden');
+            document.getElementById('wordCountValue').textContent = wordCount;
+            document.getElementById('readingSpeedValue').textContent = `${calculateReadingSpeed(startTime, endTime, wordCount)}`;
+            document.getElementById('comprehensionValue').textContent = `${comprehensionPercentage}`;
+            document.getElementById('timeResultValue').textContent = `${timeResult}`;
+            document.getElementById('results').innerHTML += '<p class="lastParagrah">Toma nota de tu velocidad de lectura para poder realizar ajustes en los próximos ejercicios.</p><p class="finalMessage">Puedes salir y pasar a la siguiente clase.</p>'
+            ;
+
+            
+            
+        }
+    });
+
+});
